08a3c580ee64f883c84ce7f4603e5898420e72c6
Charlie Culbert <charlieculbertmusic@gmail.com>
2026-09-15 13:30:53 +0200

    Query the host webview extension from init()
    The WebUI constructor ran host->get_extension from create_plugin, which
    CLAP forbids; clap-host flagged it. Call WebUI::init() from init() and
    take the char-clap-utils commit that adds it.

--- Plugin.cpp
+++ Plugin.cpp
@@ -58,6 +58,7 @@ public:
 protected:
     bool init() noexcept override
     {
+        ui.init();
         hostParams = static_cast<const clap_host_params_t*>(host->get_extension(host, CLAP_EXT_PARAMS));
         hostState = static_cast<const clap_host_state_t*>(host->get_extension(host, CLAP_EXT_STATE));
         hostPresetLoad = static_cast<const clap_host_preset_load_t*>(
